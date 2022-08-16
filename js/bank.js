72de7d3d24298eaecb1a8bb38d4670de1b23f93b
raj073 <rajoni.das@outlook.com>
2022-08-16 21:21:53 +0600

    Validation Check

--- js/bank.js
+++ js/bank.js
@@ -3,6 +3,10 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-field');
     const newDepositAmount = depositField.value;
 
+    if (isNaN(parseFloat(newDepositAmount)) || parseFloat(newDepositAmount) < 0) {
+        alert('Please Provide a Valid Number');
+    }
+
     //get the current deposit total amount
     //for non input(element other than input, textarea) use innertext to get the value
     const depositTotalElement = document.getElementById('deposit-total');
@@ -23,23 +27,37 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
 
 
 document.getElementById('btn-withdraw').addEventListener('click', function () {
+
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmount = withdrawField.value;
     //console.log(newWithdrawAmount);
 
+    if (isNaN(parseFloat(newWithdrawAmount)) || parseFloat(newWithdrawAmount) < 0) {
+        alert('Please Provide a Valid Number');
+    }
+
     const withDrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotal = withDrawTotalElement.innerText;
     //console.log(previousWithdrawTotal);
+
+    // Balance Total Updated
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+
+    withdrawField.value = '';
+    if (parseFloat(newWithdrawAmount) > parseFloat(previousBalanceTotal)) {
+        alert('You Dont have Enough Balance to Withdraw');
+        return;
+    }
 
     const updateWithdrawTotal = parseFloat(previousWithdrawTotal) + parseFloat(newWithdrawAmount);
     //console.log(updateWithdrawTotal);
     withDrawTotalElement.innerText = updateWithdrawTotal;
     withdrawField.value = '';
 
-    // Balance Total Updated
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceTotal = balanceTotal.innerText;
+
 
     const updatedBalanceAfterWithdraw = parseFloat(previousBalanceTotal) - parseFloat(newWithdrawAmount);
     balanceTotal.innerText = updatedBalanceAfterWithdraw;
+
 })
