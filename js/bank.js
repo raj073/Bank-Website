3fcf68bdde079d3e61146c8647728a895258f895
raj073 <rajoni.das@outlook.com>
2022-08-16 20:23:03 +0600

    Withdrawal updated

--- js/bank.js
+++ js/bank.js
@@ -7,18 +7,39 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     //for non input(element other than input, textarea) use innertext to get the value
     const depositTotalElement = document.getElementById('deposit-total');
     const previousDepositTotal = depositTotalElement.innerText;
-    //depositTotalElement.innerText = depositAmount;
 
     const updateDepositTotal = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
     depositTotalElement.innerText = updateDepositTotal;
     depositField.value = '';
 
     // Balance Total Updated
-
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceTotal = balanceTotal.innerText;
 
     const updatedBalanceAfterAdded = parseFloat(newDepositAmount) + parseFloat(previousBalanceTotal);
     balanceTotal.innerText = updatedBalanceAfterAdded;
 
+})
+
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmount = withdrawField.value;
+    //console.log(newWithdrawAmount);
+
+    const withDrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = withDrawTotalElement.innerText;
+    //console.log(previousWithdrawTotal);
+
+    const updateWithdrawTotal = parseFloat(previousWithdrawTotal) + parseFloat(newWithdrawAmount);
+    //console.log(updateWithdrawTotal);
+    withDrawTotalElement.innerText = updateWithdrawTotal;
+    withdrawField.value = '';
+
+    // Balance Total Updated
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+
+    const updatedBalanceAfterWithdraw = parseFloat(previousBalanceTotal) - parseFloat(newWithdrawAmount);
+    balanceTotal.innerText = updatedBalanceAfterWithdraw;
 })
